7727411f63e1502155437f0a2f673999987a571b
saulwadeleon <saul.wade@gmail.com>
2023-09-28 22:40:46 -0600

    Commit actualizacion archivos resources

--- database/foroalura.sql
+++ database/foroalura.sql
@@ -34,11 +34,11 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `foroalura`.`curso` (
   `id` BIGINT NOT NULL,
   `nombre_curso` VARCHAR(200) NOT NULL,
-  `categoriaId` BIGINT NOT NULL,
+  `categoria_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_curso_categoria_idx` (`categoriaId` ASC) VISIBLE,
+  INDEX `fk_curso_categoria_idx` (`categoria_id` ASC) VISIBLE,
   CONSTRAINT `fk_curso_categoria`
-    FOREIGN KEY (`categoriaId`)
+    FOREIGN KEY (`categoria_id`)
     REFERENCES `foroalura`.`categoria` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
@@ -87,19 +87,19 @@ CREATE TABLE IF NOT EXISTS `foroalura`.`topico` (
   `titulo_topico` VARCHAR(200) NOT NULL,
   `mensaje_topico` TEXT(600) NOT NULL,
   `fecha_creacion` DATETIME NOT NULL,
-  `autorId` BIGINT NOT NULL,
-  `cursoId` BIGINT NOT NULL,
+  `autor_id` BIGINT NOT NULL,
+  `curso_id` BIGINT NOT NULL,
   `estatus_topico` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_topico_autor_idx` (`autorId` ASC) VISIBLE,
-  INDEX `fk_topico_curso_idx` (`cursoId` ASC) VISIBLE,
+  INDEX `fk_topico_autor_idx` (`autor_id` ASC) VISIBLE,
+  INDEX `fk_topico_curso_idx` (`curso_id` ASC) VISIBLE,
   CONSTRAINT `fk_topico_autor`
-    FOREIGN KEY (`autorId`)
+    FOREIGN KEY (`autor_id`)
     REFERENCES `foroalura`.`usuario` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `fk_topico_curso`
-    FOREIGN KEY (`cursoId`)
+    FOREIGN KEY (`curso_id`)
     REFERENCES `foroalura`.`curso` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
@@ -112,20 +112,20 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `foroalura`.`respuesta` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `mensaje_respuesta` TEXT(600) NOT NULL,
-  `topicoId` BIGINT NOT NULL,
+  `topico_id` BIGINT NOT NULL,
   `fecha_respuesta` DATETIME NOT NULL,
-  `autorId` BIGINT NOT NULL,
+  `autor_id` BIGINT NOT NULL,
   `solucion` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_respuesta_topico_idx` (`topicoId` ASC) VISIBLE,
-  INDEX `fk_respuesta_autor_idx` (`autorId` ASC) VISIBLE,
+  INDEX `fk_respuesta_topico_idx` (`topico_id` ASC) VISIBLE,
+  INDEX `fk_respuesta_autor_idx` (`autor_id` ASC) VISIBLE,
   CONSTRAINT `fk_respuesta_topico`
-    FOREIGN KEY (`topicoId`)
+    FOREIGN KEY (`topico_id`)
     REFERENCES `foroalura`.`topico` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `fk_respuesta_autor`
-    FOREIGN KEY (`autorId`)
+    FOREIGN KEY (`autor_id`)
     REFERENCES `foroalura`.`usuario` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
@@ -151,8 +151,8 @@ COMMIT;
 -- Data for table `foroalura`.`usuario`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `foroalura`;
-INSERT INTO `foroalura`.`usuario` (`id`, `nombre_usuario`, `apellido_usuario`, `username`, `password`, `roleId`, `activo`) VALUES (1, 'Saul', 'Wade', 'Alura', '$2a$10$ULY.xj2PL9mBK/eEjd4uqe.Wdjxp/QYbYmqoQT1.XJ0oGwaDrGohK', 1, 1);
+USE foroalura;
+INSERT INTO `foroalura`.`usuario` (`id`, `nombre_usuario`, `apellido_usuario`, `email`, `username`, `password`, `roleId`, `activo`) VALUES (1, 'Saul', 'Wade', 'saul.wade@gmail.com', 'Alura', '$2a$10$ULY.xj2PL9mBK/eEjd4uqe.Wdjxp/QYbYmqoQT1.XJ0oGwaDrGohK', 1, 1);
 
 COMMIT;
 
